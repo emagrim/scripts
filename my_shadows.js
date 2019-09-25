e50c8c714a7a7c5eaef2765e1aa9fc41f6cddee5
emagrim <46518138+emagrim@users.noreply.github.com>
2019-09-25 12:53:34 +0200

    Add files via upload
    Test for EOBrowser

---
+++ my_shadows.js
@@ -0,0 +1,35 @@
+function evaluatePixel(samples) {
+  var sample = samples[0];
+//  if (sample != undefined){
+//  }
+//  else {lai = null;}
+//   if (((sample.B05*sample.B06*sample.B07*sample.B08*sample.B8A)*1e5)<1){
+// 	map = 0;
+// }
+// else {map = 1;}
+//   return {
+//     default: [map]
+//   }
+	return {
+		default: [sample.B02]
+	}
+}
+
+
+function setup(ds) {
+    return {
+        components: [ds.B02, ds.B03, ds.B04, ds.B05, ds.B06, ds.B07, ds.B08, ds.B8A],
+        output: [
+            {
+                id: "default",
+                sampleType: SampleType.AUTO,
+                componentCount: 1
+            }
+        ]
+    }
+}
+
+
+
+
+ // || ((sample.B02*sample.B03*sample.B04)*(sample.B03/sample.B02+sample.B04/sample.B03)*1e5)<1
