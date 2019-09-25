9bd90b4877073afa73fdc2bc47c83333ef4eee4d
emagrim <46518138+emagrim@users.noreply.github.com>
2019-09-25 12:56:44 +0200

    Add files via upload

--- my_shadows.js
+++ my_shadows.js
@@ -1,34 +1,36 @@
 //VERSION=2.1
-function evaluatePixel(samples) {
-  var sample = samples[0];
-//  if (sample != undefined){
-//  }
-//  else {lai = null;}
-//   if (((sample.B05*sample.B06*sample.B07*sample.B08*sample.B8A)*1e5)<1){
-// 	map = 0;
+return [B02*2.5,B02*2.5,B03*2.5];
+
+// function evaluatePixel(samples) {
+//   var sample = samples[0];
+// //  if (sample != undefined){
+// //  }
+// //  else {lai = null;}
+// //   if (((sample.B05*sample.B06*sample.B07*sample.B08*sample.B8A)*1e5)<1){
+// // 	map = 0;
+// // }
+// // else {map = 1;}
+// //   return {
+// //     default: [map]
+// //   }
+// 	return {
+// 		default: [sample.B02]
+// 	}
 // }
-// else {map = 1;}
-//   return {
-//     default: [map]
-//   }
-	return {
-		default: [sample.B02]
-	}
-}
 
 
-function setup(ds) {
-    return {
-        components: [ds.B02, ds.B03, ds.B04, ds.B05, ds.B06, ds.B07, ds.B08, ds.B8A],
-        output: [
-            {
-                id: "default",
-                sampleType: SampleType.AUTO,
-                componentCount: 1
-            }
-        ]
-    }
-}
+// function setup(ds) {
+//     return {
+//         components: [ds.B02, ds.B03, ds.B04, ds.B05, ds.B06, ds.B07, ds.B08, ds.B8A],
+//         output: [
+//             {
+//                 id: "default",
+//                 sampleType: SampleType.AUTO,
+//                 componentCount: 1
+//             }
+//         ]
+//     }
+// }
 
 
 
